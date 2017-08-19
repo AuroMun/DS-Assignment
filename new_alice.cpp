c337d8fb875d2e61753c537dbb731fa52cd5eadd
Sri Aurobindo Munagala <koolschoolfan@gmail.com>
2017-08-20 01:48:38 +0530

    Works!

--- new_alice.cpp
+++ new_alice.cpp
@@ -5,6 +5,7 @@
 #include<netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include<cstdlib>
 #include<string.h>
 #include<string>
 int PORT = 8080;
@@ -13,6 +14,18 @@ int accpt = 0;
 #define LENGTH 100024
 
 using namespace std;
+
+void printProgBar( int percent );
+
+int get_file_size(std::string filename) // path to file
+{
+    FILE *p_file = NULL;
+    p_file = fopen(filename.c_str(),"rb");
+    fseek(p_file,0,SEEK_END);
+    int size = ftell(p_file);
+    fclose(p_file);
+    return size;
+}
 int udp_client(){
       struct sockaddr_in si_me, si_other;
       int s, i;
@@ -25,22 +38,12 @@ int udp_client(){
       si_me.sin_family = AF_INET;
       si_me.sin_port = htons(PORT2);
       si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-      printf("Waiting to receive!\n");
       if (bind(s, (struct sockaddr *) &si_me, sizeof(si_me))==-1){
         printf("Not bound\n");
       };
-      printf("Kewl\n");
-      for (i=0; i<10; i++) {
-        printf("Hi\n");
-        if (recvfrom(s, buf, BUFSIZ, 0, (struct sockaddr *) &si_other, &slen)==-1){
-          printf("Wth\n");
-        }
-        else{
-          printf("Yay\n");
-        }
-        printf("Received packet from %s:%d\nData: %s\n\n",
-        inet_ntoa(si_me.sin_addr), ntohs(si_me.sin_port), buf);
-      }
+
+      if (recvfrom(s, buf, BUFSIZ, 0, (struct sockaddr *) &si_other, &slen)==-1);
+      printf("Received packet from %s:%d\nData: %s\n\n", inet_ntoa(si_me.sin_addr), ntohs(si_me.sin_port), buf);
 
       close(s);
 }
@@ -54,20 +57,17 @@ int udp_server(){
   if ((s=socket(AF_INET, SOCK_DGRAM, 0))==-1);
   memset( &si_other, 0, sizeof(sockaddr_in));
   si_other.sin_family = AF_INET;
-  si_other.sin_port = htons(PORT2);
+  si_other.sin_port = htons(PORT);
   si_other.sin_addr.s_addr = INADDR_ANY;
 
   //inet_aton("127.0.0.1", &si_other.sin_addr);
-  for(i=0; i<10; i++){
-    printf("Sending packet %d\n", i);
-    if(sendto(s, hello, strlen(hello), 0, (struct sockaddr *) &si_other, slen)<0){
-      printf("Failed!");
-    }
-  }
+  printf("Sending packet with message %s\n", hello);
+  if(sendto(s, hello, strlen(hello), 0, (struct sockaddr *) &si_other, slen)<0);
+
 }
 
-int server(){
 
+int server(){
     int server_fd, new_socket, valread;
     struct sockaddr_in address;
     int opt = 1;
@@ -83,14 +83,13 @@ int server(){
     bind(server_fd, (struct sockaddr *)&address, sizeof(address));
     char ch[LENGTH];
     char *fw;
-    char *sname;
+    char *sname, *tname, *foname;
     listen(server_fd, 5);
     while((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))>0){
       //fprintf(stdout, "Accepting connection\n");
     valread = read( new_socket , buffer, 1024);
     fw = strtok(buffer, ":");
-    char fname[] = "/home/doodledron/Documents/Academics/DS/new.txt";
-    FILE *fr = fopen(fname, "w");
+    char fname[] = "new.txt";
     //cout<<fw<<endl;
 
     if(!strstr(fw, "Sending")){
@@ -99,31 +98,79 @@ int server(){
       bzero(buffer, strlen(buffer));
     }
     else{
-      sname = strtok(0, " ");
+      sname = strtok(0, ":");
+      FILE *fr = fopen(fname, "w");
       fprintf(stdout, "Receiving file %s\n>>", sname);
+      int fsz, fsz_copy;
+      tname = strtok(0, ":");
+      foname = strtok(0, ":");
+      fsz = atoi(foname);
+      fsz_copy = fsz;
+      if(strstr(tname, "TCP")){
+        bzero(buffer, LENGTH);
+        int fr_block_sz = 0;
 
-      bzero(buffer, LENGTH);
-      int fr_block_sz = 0;
-      if(!fr){
-        fprintf(stdout, "LOSER \n");
-      }
-      else cout<<fr<<endl;
-
-      while(1)
-      {
-        fr_block_sz = recv(new_socket, buffer, LENGTH, 0);
-        if(fr_block_sz > 0){
-          int write_sz = fwrite(buffer, sizeof(char), fr_block_sz, fr);
-          printf("%s\n", buffer);
-          bzero(buffer, LENGTH);
+        while(1)
+        {
+          fr_block_sz = recv(new_socket, buffer, LENGTH, 0);
+          if(fsz > 0){
+            int write_sz = fwrite(buffer, sizeof(char), fr_block_sz, fr);
+            //printf("%s\n", buffer);
+            fsz -= fr_block_sz;
+            if(fsz<0)fsz=0;
+            int pN = (fsz_copy-fsz)*100.0/fsz_copy;
+            //printf("%d %d %d %d\n", fsz_copy, fsz, fsz_copy-fsz, pN);
+            printProgBar(pN);
+            bzero(buffer, LENGTH);
+          }
+          else break;
         }
-        else break;
+        fprintf(stdout, "\nReceived file, wrote to %s \n>>", fname);
+        fclose(fr);
+        close(new_socket);
       }
-      fprintf(stdout, "Received file, wrote to %s \n", fname);
-      fclose(fr);
-      fprintf(stdout, "Closed file %s\n", fname);
+      else{
+        close(new_socket);
+        int fr_block_sz = 0;
+        struct sockaddr_in si_me, si_other;
+        int s, i;
+        unsigned int slen=sizeof(si_other);
+        char buf[BUFSIZ];
+
+        if ((s=socket(AF_INET, SOCK_DGRAM, 0))==-1);
+
+        memset(&si_me, 0, sizeof(si_me));
+        si_me.sin_family = AF_INET;
+        si_me.sin_port = htons(PORT2);
+        si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+        if (bind(s, (struct sockaddr *) &si_me, sizeof(si_me))==-1){
+          printf("Not bound\n");
+        };
+        bzero(buf, BUFSIZ);
+        printf("Made new socket to receive:\n");
+        //if (recvfrom(s, buf, BUFSIZ, 0, (struct sockaddr *) &si_other, &slen)==-1);
+        while(1)
+        {
+          fr_block_sz = recvfrom(s, buf, BUFSIZ, 0, (struct sockaddr *) &si_other, &slen);
+          fsz -= fr_block_sz;
+          if(fsz<0)fsz=0;
+          int pN = (fsz_copy-fsz)*100.0/fsz_copy;
+          //printf("%d %d %d %d\n", fsz_copy, fsz, fsz_copy-fsz, pN);
+          printProgBar(pN);
+          fwrite(buf, sizeof(char), fr_block_sz, fr);
+          if(fsz > 0){
+            bzero(buf, BUFSIZ);
+          }
+          else break;
+
+        }
+        fclose(fr);
+        fprintf(stdout, "\nDone. Print Enter to Continue\n>>");
+        //printf("Received packet from %s:%d\nData: %s\n\n", inet_ntoa(si_me.sin_addr), ntohs(si_me.sin_port), buf);
+
+        close(s);
+      }
     }
-    close(new_socket);
     //fprintf(stdout, "Closed connection\n");
   }
   return 0;
@@ -132,50 +179,84 @@ int server(){
 int client(){
   if(accpt==0)return 0;
   while(1){
-    char ha[LENGTH];
+    char ha[LENGTH], cop[LENGTH];
     cout<<">>";
+    cin>>ha;
 
     struct sockaddr_in address;
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char *sname;
+    char *sname, *tname;
     char buffer[LENGTH] = {0};
     sock = socket(AF_INET, SOCK_STREAM, 0);
     memset(&serv_addr, '0', sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT2);
     inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
-    cin>>ha;
     fprintf(stdout, "Ha: %s\n", ha);
     connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
 
-    send(sock , ha , strlen(ha) , 0 );
+
     int fs_block_sz;
     char *fw;
+    strcpy(cop, ha);
     fw = strtok(ha, ":");
     if(!strstr(fw, "Sending")){
-    //if(1){
-        //send(sock, ha, strlen(ha), 0);
+        send(sock , ha , strlen(ha) , 0 );
         bzero(ha, strlen(ha));
-        //fprintf(stdout, "Sent a message! %s\n ", ha);
         close(sock);
-        //fprintf(stdout, "Closed connection\n");
     }
     else{
-      sname = strtok(0, " ");
-      fprintf(stdout, "File %s\n", sname);
-
-      FILE *fp = fopen(sname, "r");
-      bzero(buffer, LENGTH);
-
-      while((fs_block_sz = fread(buffer, sizeof(char), LENGTH, fp)) > 0)
-      {
-        if(send(sock, buffer, fs_block_sz, 0) < 0);
+      sname = strtok(0, ":");
+      fprintf(stdout, "File: %s\n", sname);
+      tname = strtok(0, ":");
+      fprintf(stdout, "Mode: %s\n", tname);
+      int sz = get_file_size(sname);
+      strcat(cop, ":");
+      char z[100];
+      sprintf(z, "%d", sz);
+      strcat(cop, z);
+      printf("%s\n", cop);
+      send(sock , cop , strlen(cop) , 0 );
+      if(strstr(tname, "TCP")){
+        FILE *fp = fopen(sname, "r");
         bzero(buffer, LENGTH);
+        while((fs_block_sz = fread(buffer, sizeof(char), LENGTH, fp)) > 0)
+        {
+          if(send(sock, buffer, fs_block_sz, 0) < 0);
+          bzero(buffer, LENGTH);
+        }
+        fclose(fp);
+        close(sock);
       }
-      fclose(fp);
+      else{
+        close(sock);
+        sleep(1);
+        struct sockaddr_in si_other;
+        int s, i, slen=sizeof(si_other);
+        char buf[BUFSIZ];
+        if ((s=socket(AF_INET, SOCK_DGRAM, 0))==-1);
+        memset( &si_other, 0, sizeof(sockaddr_in));
+        si_other.sin_family = AF_INET;
+        si_other.sin_port = htons(PORT);
+        si_other.sin_addr.s_addr = INADDR_ANY;
+
+        //printf("Sending packet with message %s\n", hello);
+        //if(sendto(s, hello, strlen(hello), 0, (struct sockaddr *) &si_other, slen)<0);
+        //cin>>ha;
+        FILE *fp = fopen(sname, "r");
+        bzero(buf, BUFSIZ);
+        while((fs_block_sz = fread(buf, sizeof(char), BUFSIZ, fp)) > 0)
+        {
+          if(sendto(s, buf, BUFSIZ, 0, (struct sockaddr *) &si_other, slen)<0);
+          //printf("Sending\n");
+          usleep(1000);
+          bzero(buf, BUFSIZ);
+        }
+        fclose(fp);
+        printf("Ooh Sent!\n");
+      }
     }
-    close(sock);
   }
   return 0;
 }
@@ -185,9 +266,27 @@ int main(int argc, char const *argv[]){
   PORT = atoi(argv[1]);
   PORT2 = atoi(argv[2]);
   accpt = atoi(argv[3]);
-  thread first(udp_server);
-  thread second(udp_client);
+  thread first(server);
+  thread second(client);
   first.join();
   second.join();
   return 0;
+}
+
+void printProgBar( int percent ) {
+ string bar;
+
+ for(int i = 0; i < 50; i++){
+   if( i < (percent/2)){
+     bar.replace(i,1,"=");
+   }else if( i == (percent/2)){
+     bar.replace(i,1,">");
+   }else{
+     bar.replace(i,1," ");
+   }
+ }
+
+ cout<< "\r" "[" << bar << "] ";
+ cout.width( 3 );
+ cout<< percent << "%     " << std::flush;
 }
